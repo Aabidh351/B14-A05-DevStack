b5bd79a596ea2e5955a23ea38b045f87d6ca2ee7
Aabidh351 <aabidh351@gmail.com>
2026-09-13 17:27:22 +0600

    Added JSON data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Explore from './components/Explore'
 import Hero from './components/HeroBanner'
 import Navbar from './components/Navbar'
 
@@ -8,6 +9,7 @@ function App() {
     <>
     <Navbar/>
     <Hero/>
+    <Explore/>
     </>
   )
 }
---
+++ src/components/Explore.tsx
@@ -0,0 +1,9 @@
+const Explore = () =>{
+    return <>
+    <h2>
+        Explore the Technologies
+    </h2>
+    </>
+}
+
+export default Explore;
