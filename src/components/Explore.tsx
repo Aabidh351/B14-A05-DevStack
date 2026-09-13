8e3250abb2d7235904b74354a3ec2501a468acc1
Aabidh351 <aabidh351@gmail.com>
2026-09-13 18:04:47 +0600

    data fetched and loaded

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -1,9 +1,37 @@
-const Explore = () =>{
-    return <>
-    <h2>
-        Explore the Technologies
-    </h2>
-    </>
-}
+import { useEffect, useState } from "react";
+import type { TechType } from "../types/TechType";
+
+
+const Explore = () => {
+  const [tech, setTech] = useState<TechType[]>([]);
+
+  useEffect(() => {
+    const fetchTech = async () => {
+      const response = await fetch("/API/tech-data.json");
+      const data = await response.json();
+
+      setTech(data);
+    };
+
+    fetchTech();
+  }, []);
+
+  console.log(tech);
+
+  return (
+    <section>
+      <h2>Explore the Technologies</h2>
+
+      <div>
+        {tech.map((technology) => (
+          <div key={technology.id}>
+            <h3>{technology.name}</h3>
+            <p>{technology.description}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default Explore;
